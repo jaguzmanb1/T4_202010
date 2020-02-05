5e46a85b65e364e0912d50d7c60b0b043e7e22c3
Jhon Guzmán <ja.guzmab1@uniandes.edu.co>
2020-02-05 10:16:43 -0500

    Pruebas unitarias

--- test/test/logic/TestModelo.java
+++ test/test/logic/TestModelo.java
@@ -1,25 +1,25 @@
 package test.logic;
 
 import static org.junit.Assert.*;
+import model.logic.Comparendo;
 import model.logic.Modelo;
 
 import org.junit.Before;
 import org.junit.Test;
 
 public class TestModelo {
-	
+
 	private Modelo modelo;
 	private static int CAPACIDAD=100;
-	
+
 	@Before
 	public void setUp1() {
-		modelo= new Modelo(CAPACIDAD);
+		modelo= new Modelo();
 	}
 
 	public void setUp2() {
-		for(int i =0; i< CAPACIDAD;i++){
-			modelo.agregar(i);
-		}
+		modelo.cargarDatos();;
+
 	}
 
 	@Test
@@ -29,39 +29,17 @@ public class TestModelo {
 		assertEquals(0, modelo.darTamano());  // Modelo con 0 elementos presentes.
 	}
 
-	@Test
-	public void testDarTamano() {
-		// TODO
-		setUp2();
-		assertEquals(100, modelo.darTamano());
-	}
-
-	@Test
-	public void testAgregar() {
-		// TODO Completar la prueba
-		Integer prueba = 100;
-		modelo.agregar(prueba);
-		assertEquals(prueba, modelo.buscar(prueba));
-		
-	}
 
 	@Test
 	public void testBuscar() {
 		setUp2();
-		// TODO Completar la prueba
-		assertEquals((Integer)41, modelo.buscar(41));
-		assertEquals((Integer)24, modelo.buscar(24));
+		Comparendo comparendo = new Comparendo (29042, null, null, null, null, null, null);
+		// TODO Completar la prueba 29042
+		assertEquals(null, modelo.buscar(41));
+		assertEquals("29042 ", modelo.buscar(29042).split("-")[0]);
+
 
 	}
-
-	@Test
-	public void testEliminar() {
-		setUp2();
-		assertEquals((Integer)41, modelo.eliminar(41));
-		assertEquals(null, modelo.eliminar(41));
-		
-		// TODO Completar la prueba
-		
-	}
-
 }
+
+
