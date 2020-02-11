f035d8239927bec332d446279bee12aa7ab70d60
JuanLugoS <jd.lugo@uniandes.edu.co>
2020-02-11 09:14:58 -0500

    Termino IStack y Stack, Creacion de StackVacioException

--- src/model/data_structures/Stack.java
+++ src/model/data_structures/Stack.java
@@ -1,12 +1,12 @@
 package model.data_structures;
 
-import java.util.Iterator;
+import java.util.*;
 
 //import com.sun.tools.classfile.Annotation.element_value;
 
 //import sun.net.www.content.text.plain;
 
-public class Stack<T>{
+public class Stack<T> implements IStack<T> {
 	
 	private NodoStack<T> primerElemeto;
 	
@@ -80,12 +80,30 @@ public class Stack<T>{
 			public T next() {
 				// TODO Auto-generated method stub
 				if(!hasNext())
-					throw new Exception("s");
+					throw new NoSuchElementException();
 
 				NodoStack<T> item = current;
 				current = current.darSiguiente();
 				return item.darElemento();
 			}
 		};
+	}
+
+	@Override
+	public Iterator<T> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 }
---
+++ src/model/data_structures/StackVacioException.java
@@ -0,0 +1,17 @@
+package model.data_structures;
+
+
+public class StackVacioException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public StackVacioException(String pMensaje) 
+	{
+		super(pMensaje);
+		// TODO Auto-generated constructor stub
+	}
+
+}
